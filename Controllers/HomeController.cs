b270fdc37b050d4676db6e00131d3a167e283df9
$HIVAM <supshiv7250@gmail.com>
2024-06-14 03:57:27 +0530

    minor updates

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using RF_Technologies.Data_Access.Repository.IRepository;
+using RF_Technologies.Model;
 using RF_Technologies.Models;
 using System.Diagnostics;
 
@@ -7,10 +9,12 @@ namespace RF_Technologies.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
@@ -26,6 +30,20 @@ namespace RF_Technologies.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(Contact obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Contact.Add(obj);
+                _unitOfWork.Save();
+                return RedirectToAction("Index", "Home");
+            }
+            else { 
+                return View();
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
