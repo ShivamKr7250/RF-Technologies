447a1d2f06891729b80a2bd25be426a4275a8614
Shivam <supshiv7250@gmail.com>
2025-02-13 12:56:03 +0530

    some changes

--- RF Technologies.Data Access/obj/Debug/net8.0/RF Technologies.Data Access.AssemblyInfo.cs
+++ RF Technologies.Data Access/obj/Debug/net8.0/RF Technologies.Data Access.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RF Technologies.Data Access")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+808194e690a49ded97276f21e7375f0e429ec17f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c6e0603ea8be71ca74e6b5399ee5b6c9614ae3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("RF Technologies.Data Access")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RF Technologies.Data Access")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
